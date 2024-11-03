7f347c7411fd5e8d63bea1f56598e14fb0fdba63
Swellmary <suarezwellmary@gmail.com>
2024-11-03 23:17:52 +0800

    first commit

---
+++ Login.php
@@ -0,0 +1,24 @@
+<?php
+    include"./connectiondb.php";
+    include"./includes/header.php"
+?>
+
+
+<div class="container">
+    <div class="form">
+        <h1>Evami's Portfolio</h1>
+        <p>enter your account</p>
+        <div>
+            <input type="text" name="email" placeholder="Email">
+        </div>
+        <div>
+            <input type="text" name="password" placeholder="Password">
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<p>You've reached the end. <a href="index.php">Click here</a> to go back to top</p>
+</body>
+</html>
